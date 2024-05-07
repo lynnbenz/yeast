d71f412ef8991b5471052624406adc5292de5e10
lynnbenz <lynn.benz@stud.fhgr.ch>
2024-05-07 20:15:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,9 +13,74 @@ async function fetchData() {
 }
 
 async function main() {
-
 let data = await fetchData();
-console.log(data);
+let date = data.date;
+let location = data.location;
+let lastValue = data.lastValue;
+
+console.log ('Date: ' + date);
+console.log ('Location: ' + location);      
+console.log ('Last Value: ' + lastValue);
+
+
+
+
+const ctx = document.getElementById('myChart').getContext('2d');
+
+async function fetchData() {
+    try {
+        const response = await fetch('https://599768-3.web.fhgr.ch/endpoint.php');
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+function createLineChart(labels, data) {
+    new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Last Value',
+                data: data,
+                borderColor: 'blue',
+                fill: false
+            }]
+        },
+        options: {
+            responsive: true,
+            scales: {
+                x: {
+                    display: true,
+                    title: {
+                        display: true,
+                        text: 'Date'
+                    }
+                },
+                y: {
+                    display: true,
+                    title: {
+                        display: true,
+                        text: 'Last Value'
+                    }
+                }
+            }
+        }
+    });
+}
+
+async function main() {
+    let data = await fetchData();
+    let date = data.date;
+    let lastValue = data.lastValue;
+
+    console.log('Date: ' + date);
+    console.log('Last Value: ' + lastValue);
+
+    createLineChart(date, lastValue);
 }
 
 main();
+}
